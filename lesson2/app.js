720ede7cf37471e4e98ee06d3e85c8ac8bb538a6
minghei209 <minghei209@gmail.com>
2019-01-19 11:14:32 +0800

    first push

---
+++ lesson2/app.js
@@ -0,0 +1,17 @@
+console.log("test....");
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(0,0);
+ctx.lineTo(200,100);
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.stroke();
+
+ctx.fillStyle = 'rgb(200, 50, 10)';
+ctx.fillRect(75,125,150,100);
+
+ctx.strokeStyle = 'green';
+ctx.strokeRect(50,100,200,150);
+
+ctx.clearRect(125,150,50,50);
