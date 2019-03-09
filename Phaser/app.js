812967268f5dc8d609782cf68ea4be87148f1c12
minghei209 <minghei209@gmail.com>
2019-03-09 11:47:46 +0800

    Phaser

--- Phaser/app.js
+++ Phaser/app.js
@@ -4,7 +4,7 @@ var life = 3;
 
 function preload(){
 	game.load.image('sky','assets/sky.png');
-	game.load.image('groung','assets/platform.png');
+	game.load.image('ground','assets/platform.png');
 	game.load.image('star','assets/star.png');
 	game.load.spritesheet('dude','assets/dude.png', 32, 48);
 	game.load.spritesheet('baddie','assets/baddie.png', 32, 32);
@@ -33,5 +33,93 @@ function create(){
 	lifelabel.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
 	lifetext.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
 
+player = game.add.sprite(32, 400, 'dude');
+player.animations.add('left', [0,1,2,3], 10, true);
+player.animations.add('right', [5,6,7,8], 10, true);
+game.physics.arcade.enable(player);
+player.body.bounce.y = 0.2;
+player.body.gravity.y = 300;
+player.body.collideWorldBounds = true;
+
+enemy1 = game.add.sprite(32, 400, 'baddie');
+enemy1.animations.add('left', [0,1], 10, true);
+enemy1.animations.add('right', [2,3], 10, true);
+game.physics.arcade.enable(enemy1);
+enemy1.body.bounce.y = 0.2;
+enemy1.body.gravity.y = 300;
+enemy1.body.collideWorldBounds = true;
+
+stars = game.add.physicsGroup();
+stars.enableBody = true;
+for(var i=0; i<12; i++){
+	var star = stars.create(i*70, 0, 'star');
+	star.body.gravity.y = 200;
+	star.body.bounce.y = 0.7 + Math.random()*0.2;
+	}
+
+	cursors = game.input.keyboard.createCursorKeys();
 }
 
+function update(){
+	game.physics.arcade.collide(player, platforms);
+	game.physics.arcade.collide(stars, platforms);
+	game.physics.arcade.collide(enemy1, platforms);
+
+	player.body.velocity.x = 0;
+
+	if(cursors.left.isDown){
+		player.body.velocity.x = -150;
+		player.animations.play('left');
+	} else if(cursors.right.isDown){
+		player.body.velocity.x = 150;
+		player.animations.play('right');
+	} else {
+		player.animations.stop();
+		player.frame = 4;
+	}
+
+	if(cursors.up.isDown && player.body.touching.down){
+		player.body.velocity.y = -300;
+	}
+
+	game.physics.arcade.overlap(player, stars, collectStar);
+	game.physics.arcade.overlap(player, enemy1, loseLife);
+
+	moveEnemy();
+
+	if(life < 0){
+		endGame()
+	}
+}
+
+function collectStar(player, star){
+	score = score + 1;
+	scoretext.setText(score);
+	star.kill();
+	star.reset(Math.floor(Math.random()*750),0)
+}
+
+function loseLife(player, enemy1){
+	life -= 1;
+	lifetex.setText(life);
+	enemy1.kill();
+	enemy1.reset(10,20)
+}
+
+function moveEnemy(){
+	if(enemy1.x > 759){
+		enemy1.animations.play('left');
+		enemy.body.velocity.x = -120;
+	} else if (enemy1.x < 405){
+		enemy1.animations.play('right')
+		enemy.body.velocity.x = 120;
+	}
+}
+
+function endGame(){
+	player.kill();
+	scorelabel.text = "GAME OVER! You scored " + score;
+scoretext.visible = false;
+lifelabel.visible = false;
+lifetext.visible = false;
+}
